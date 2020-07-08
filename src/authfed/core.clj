3a03b785480649fd051dd91603246b57925b22ce
James Davidson <jamesd3142@gmail.com>
2020-07-08 22:06:24 +0800

    Get iOS to trigger 1Password prompt

--- src/authfed/core.clj
+++ src/authfed/core.clj
@@ -96,6 +96,10 @@
                     (template/input {:id "mobile"
                                      :type "text"
                                      :label "Mobile"})
+                    (-> (template/input {:id "password"
+                                         :type "password"
+                                         :label "Password"})
+                     (update :attrs assoc :style "display:none;"))
                     (template/input {:id "submit"
                                      :type "submit"
                                      :classes ["btn" "btn-primary"]
